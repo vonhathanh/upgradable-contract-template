740ab21d9bee29578288507e0c42daf92d30ff2b
hanhvn <vonhathanh@gmail.com>
2022-06-10 13:18:01 +0700

    nothing to commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,7 +22,7 @@ module.exports = {
   networks: {
     hardhat: {},
     bsctest: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      url: "https://data-seed-prebsc-2-s1.binance.org:8545/",
       accounts: [process.env.PRIVATE_KEY],
     },
   },
